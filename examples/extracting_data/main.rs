77dbd08484bd537e7fab9ead710a8b988db45ca2
raimundo saona <rasa200@gmail.com>
2020-08-28 14:52:41 +0200

    Corrected retrieved data

--- examples/extracting_data/main.rs
+++ examples/extracting_data/main.rs
@@ -2,14 +2,14 @@ use std::fs::File;
 use std::io::{Write, BufReader, BufRead, Error};
 
 fn main() -> Result<(), Error> {
-    let target_path = "target/preexplorer/data/sandpiper_poly_all.txt";
-    // let target_path = "target/preexplorer/data/redneck_poly_all.txt";
+    // let target_path = "target/preexplorer/data/sandpiper_poly_all.txt";
+    let target_path = "target/preexplorer/data/redneck_poly_all.txt";
     let mut output = File::create(target_path)?;
 
     // Getting data
-    for counter in 1..=1000 {
-        let source_path = format!("target/preexplorer/data/sandpiper_poly_{}.txt", counter);
-        // let source_path = format!("target/preexplorer/data/redneck_poly_{}.txt", counter);
+    for counter in 1..=10000 {
+        // let source_path = format!("target/preexplorer/data/sandpiper_poly_{}.txt", counter);
+        let source_path = format!("target/preexplorer/data/redneck_poly_{}.txt", counter);
 	    let input = File::open(source_path)?;
 	    let buffered = BufReader::new(input);
 	    for line in buffered.lines().skip(3) {

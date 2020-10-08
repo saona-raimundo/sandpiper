c6957830414eb59725b754d377ea5eaf8456e921
Raimundo Saona <rasa200@gmail.com>
2020-10-08 21:59:54 +0200

    Computations done

--- examples/expected_polymorphisms/main.rs
+++ examples/expected_polymorphisms/main.rs
@@ -112,7 +112,7 @@ fn main() {
 		println!("The computations took {} secs.", now.elapsed().as_secs());
 	}
 	// Approximation for a grid set of parameters
-	if true {	
+	if false {	
 		// Parameters
 		// mu
 		let locations = vec![-0.2000000000, -0.1523191619, -0.1160056354, -0.0883494058, -0.0672865373, -0.0512451448, -0.0390280876, -0.0297236279, -0.0226373905, -0.0172405417, -0.0131303243, -0.0100000000];
@@ -204,10 +204,10 @@ fn main() {
 		}
 	}
 	// Approximation of conditional expectation for a grid set of parameters
-	if false {	
+	if true {	
 		// Parameters
 		// mu
-		let locations = vec![-0.2000000000, -0.1523191619, -0.1160056354, -0.0883494058, -0.0672865373, -0.0512451448, -0.0390280876, -0.0297236279, -0.0226373905, -0.0172405417, -0.0131303243, -0.0100000000];
+		let locations = vec![-0.0100000000, -0.0056838040, -0.0032305627, -0.0018361885, -0.0010436536, -0.0005931922, -0.0003371588, -0.0001916345, -0.0001089213, -0.0000619087, -0.0000351877, -0.0000200000];
 		// sigma
 		let scales = vec![0.0000100000, 0.0000187382, 0.0000351119, 0.0000657933, 0.0001232847, 0.0002310130, 0.0004328761, 0.0008111308, 0.0015199111, 0.0028480359, 0.0053366992, 0.0100000000];
 		// let shapes_and_rates = vec![(0., 1000.), (-2.5, 1000.), (0., 7000.)]; // 
@@ -217,16 +217,17 @@ fn main() {
 		let rates = vec![0., 10., 50., 100.,  1000., 5000.]; // vec![1e3]; // 
 		
 		// Conditional expectation bounds
-		let lower_bound = -0.01;
-		let upper_bound = 0.0005;
+		let lower_bound = -1.0;
+		let upper_bound = 1.0;
 
 		// Computational parameters
 		let variance_samples = 1000;
 		let error_limit = 1e-6;
 
 		// Computing redneck
-		if false {
+		if true {
 			let (start, end) = (1, u64::MAX); //(locations.len() * scales.len() * shapes.len() * rates.len()) as u64); // 2000);
+			println!("Redneck");
 			let mut data = Vec::new();
 			let mut counter = 0;
 			let progress_bar = ProgressBar::new(u64::min(end, (locations.len() * scales.len() * shapes.len() * rates.len()) as u64) + 1 - start).with_style(
@@ -262,8 +263,9 @@ fn main() {
 		}
 
 		// Computing sandpiper
-		if false {
+		if true {
 			let (start, end) = (1, u64::MAX); //(locations.len() * scales.len() * shapes.len() * rates.len()) as u64); // 2000);
+			println!("Sandpiper");
 			let mut data = Vec::new();
 			let mut counter = 0;
 			let progress_bar = ProgressBar::new(u64::min(end, (locations.len() * scales.len() * shapes.len() * rates.len()) as u64) + 1 - start).with_style(

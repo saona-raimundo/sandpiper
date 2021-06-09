24802dce5d08f3a88280d51b39de59f3e86ee902
raimundo saona <rasa200@gmail.com>
2021-06-09 10:24:38 +0200

    Ready a cluster run

--- examples/cluster/run_script.sh
+++ examples/cluster/run_script.sh
@@ -1,11 +1,9 @@
-#!/bin/bash
-#
 #-------------------------------------------------------------
-#example script for running a single-CPU serial job via SLURM
+#script for running a single-CPU serial job via SLURM
 #-------------------------------------------------------------
 #
-#SBATCH --job-name=YourJobNameHere
-#SBATCH --output=YourOutputFileNameHere
+#SBATCH --job-name=sandpiper
+#SBATCH --output=sandpiper_output
 #
 #Define the number of hours the job should run. 
 #Maximum runtime is limited to 10 days, ie. 240 hours
@@ -15,27 +13,23 @@
 #SBATCH --mem=2G
 #
 #Send emails when a job starts, it is finished or it exits
-#SBATCH --mail-user=YourEmail@ist.ac.at
+#SBATCH --mail-user=raimundojulian.saonaurmeneta@ist.ac.at
 #SBATCH --mail-type=ALL
 #
-#Pick whether you prefer requeue or not. If you use the --requeue
-#option, the requeued job script will start from the beginning, 
-#potentially overwriting your previous progress, so be careful.
-#For some people the --requeue option might be desired if their
-#application will continue from the last state.
-#Do not requeue the job in the case it fails.
+#Pick whether you prefer requeue or not. 
+# Options: --requeue or --no-requeue
 #SBATCH --no-requeue
 #
 #Do not export the local environment to the compute nodes
 #SBATCH --export=NONE
 unset SLURM_EXPORT_ENV
 #
-#for single-CPU jobs make sure that they use a single thread
+#Make sure that they use a single thread
 export OMP_NUM_THREADS=1
 #
 #load the respective software module you intend to use
-module load YourModuleHere
+module load rust
 #
 #
 #run the respective binary through SLURM's srun
-srun --cpu_bind=verbose  YourExecutableHere  /Path/To/Your/Input/Files/Here
+srun --cpu_bind=verbose  cluster.exe

b014df10a630190a756e3e355ac8c297a4f7d22a
raimundo saona <rasa200@gmail.com>
2021-06-11 10:40:29 +0200

    From pc

--- examples/cluster/cluster.d
+++
@@ -1,5 +0,0 @@
-C:\Users\rsaonaur\projects\sandpiper\target\release\examples\cluster.exe: examples\cluster\main.rs
-
-C:\Users\rsaonaur\projects\sandpiper\target\release\examples\cluster.d: examples\cluster\main.rs
-
-examples\cluster\main.rs:
